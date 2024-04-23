6203d8663a6da0d70245c4d76ba532266c1952f8
Itttikorn <ittikornsd@gmail.com>
2024-04-23 13:53:51 +0700

    test

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,7 @@
-const { dayOfTheWeek, monthOfTheYear } = require('./app');
+const { dayOfTheWeek } = require('./app');
 
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
-
-test('getMonth returns the long-format month of the year', () => {
-    const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('March');
-}); 
